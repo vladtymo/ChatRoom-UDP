a5404c32d1ac679afb92940fd9139557984b9936
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-09-30 18:36:57 +0300

    Change server port to 4041.

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -8,7 +8,7 @@ namespace ServerApp
 {
     public class ChatServer
     {
-        const short port = 4040;
+        const short port = 4041;
         const string JOIN_CMD = "$<join>";
 
         IPEndPoint clientEndPoint = null;
